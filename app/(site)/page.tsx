2e5e26bda21c4ca64d229a43a6a9fc90de00a8d2
crsd <vovalitvinov123lalala@gmail.com>
2025-12-14 01:48:27 +0200

    fix: build errors

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,8 @@
 import { ProjectsInfiniteList } from '@/components/ProjectsInfiniteList'
 import { ProjectsListServer } from '@/components/ProjectsListServer'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   return (
     <>
