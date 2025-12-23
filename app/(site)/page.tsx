2a3c3ee7d894ec5ac35209b1ceda3f222edc36e3
crsd <vovalitvinov123lalala@gmail.com>
2025-12-23 23:10:28 +0200

    fix: build errors

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link'
 import { AuthorsList } from '@/components/AuthorsList'
 import { HeaderMain } from '@/components/HeaderMain'
 
+export const dynamic = 'force-dynamic'
+
 export default async function Home() {
   return (
     <div className="max-w-2xl mx-auto">
